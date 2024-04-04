fc525b297fb2824ef28fecdb61f8121369cea729
ccolkereybel <163072600+ccolkereybel@users.noreply.github.com>
2024-04-03 20:21:15 -0400

    day 2 databases

--- Labs/Databases/lab_01 selects.sql
+++ Labs/Databases/lab_01 selects.sql
@@ -1,22 +1,22 @@
--- SELECT invoice_number, invoice_total-payment_total-credit_total AS "Balance Due", payment_date
--- FROM invoices
--- WHERE payment_date is NULL
--- ORDER BY invoice_total-payment_total-credit_total DESC
+SELECT invoice_number, invoice_total-payment_total-credit_total AS "Balance Due", payment_date
+FROM invoices
+WHERE payment_date is NULL
+ORDER BY invoice_total-payment_total-credit_total DESC;
 
--- SELECT 60000 AS Principle, 60000* .065 AS Interest, 60000*1.065 AS Total 
+SELECT 60000 AS Principle, 60000* .065 AS Interest, 60000*1.065 AS Total; 
 
--- SELECT NOW() AS "CURRENT TIME"
+SELECT NOW() AS "CURRENT TIME";
 
--- SELECT CURRENT_TIME
+SELECT CURRENT_TIME;
 
--- SELECT vendor_name, vendor_state
--- FROM   vendors
--- ORDER BY vendor_state
+SELECT vendor_name, vendor_state
+FROM   vendors
+ORDER BY vendor_state;
 
--- SELECT vendor_name, vendor_state
--- FROM   vendors
--- WHERE NOT vendor_state in ("OH", "PA", "IN")
--- ORDER BY vendor_state
+SELECT vendor_name, vendor_state
+FROM   vendors
+WHERE NOT vendor_state in ("OH", "PA", "IN")
+ORDER BY vendor_state;
 
 SELECT invoice_number, invoice_date
 FROM invoices
