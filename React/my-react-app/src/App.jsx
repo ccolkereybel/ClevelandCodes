99ca6a2cad5d455f3e677417135c5a5b5af688e9
Cara Colker-Eybel <ccolkereybel@gmail.com>
2024-09-18 12:32:18 -0400

    updated notes

--- React/my-react-app/src/App.jsx
+++ React/my-react-app/src/App.jsx
@@ -1,13 +1,9 @@
-import Header from "./Header.jsx";
-import Footer from "./Footer.jsx";
-import Food from "./Food.jsx";
+import UserGreeting from "./UserGreeting.jsx";
 
 function App() {
   return (
     <>
-      <Header />
-      <Food />
-      <Footer />
+      <UserGreeting isLoggedIn={true} username="BroCode"/>
     </>
   );
 }
