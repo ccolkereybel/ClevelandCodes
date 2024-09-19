5f52ded6c6813b144534439a1b94c74708d2d846
Cara Colker-Eybel <ccolkereybel@gmail.com>
2024-09-19 12:05:07 -0400

    notes practice

--- React/my-react-app/src/App.jsx
+++ React/my-react-app/src/App.jsx
@@ -4,10 +4,14 @@ import List from "./List.jsx";
 
 function App() {
 
+  const fruits = [{id: 1, name: "apple", calories: 10}, 
+                  {id: 2, name: "orange", calories: 50},
+                  {id: 3, name: "banana", calories: 100}]
+
 
   return (
     <>
-  <List />
+  <List items={fruits} category="Fruits"/>
     </>
   );
 }
