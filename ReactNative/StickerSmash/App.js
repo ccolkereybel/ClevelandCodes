a13a2f30fb22350c5c4d414e069ee2613da1b62d
ccolkereybel <163072600+ccolkereybel@users.noreply.github.com>
2024-10-01 17:46:03 -0400

    notes

--- ReactNative/StickerSmash/App.js
+++ ReactNative/StickerSmash/App.js
@@ -9,6 +9,7 @@ import ImageViewer from "./Components/ImageViewer";
 const PlaceholderImage = require("./assets/images/background-image.png");
 
 export default function App() {
+  const [showAppOptions, setShowAppOptions] = useState(false);
   const [selectedImage, setSelectedImage] = useState(null);
   const pickImageAsync = async () => {
     let result = await ImagePicker.launchImageLibraryAsync({
@@ -18,6 +19,7 @@ export default function App() {
 
     if (!result.canceled) {
       setSelectedImage(result.assets[0].uri);
+      setShowAppOptions(true);
     } else {
       alert("You did not select any image.");
     }
@@ -36,8 +38,26 @@ export default function App() {
           label="Choose a photo"
           onPress={pickImageAsync}
         />
-        <Button label="Use this photo" />
+        <Button
+          label="Use this photo"
+          onPress={() => setShowAppOptions(true)}
+        />
       </View>
+      {showAppOptions ? (
+        <View />
+      ) : (
+        <View style={styles.footerContainer}>
+          <Button
+            theme="primary"
+            label="Choose a photo"
+            onPress={pickImageAsync}
+          />
+          <Button
+            label="Use this photo"
+            onPress={() => setShowAppOptions(true)}
+          />
+        </View>
+      )}
       <StatusBar style="auto" />
     </View>
   );
